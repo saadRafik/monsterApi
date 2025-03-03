359f78a72610af51ce9abd8b7d18e690737dfcb6
Saad <saadrafik75@gmail.com>
2025-03-03 09:07:56 +0100

    Base

---
+++ src/test/java/monstre/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package monstre.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
